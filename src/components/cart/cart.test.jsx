06d894313a07e213cf2fd3766f8a2d6f12e1c32c
newerk <juniusowens@yahoo.com>
2023-11-01 13:08:42 -0400

    Flesh Out Code for Increment/Decrement Test

--- src/components/cart/cart.test.jsx
+++ src/components/cart/cart.test.jsx
@@ -1,6 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
+
+const mockIncrement = vi
+  .fn()
+  .mockImplementation((quantity) => parseInt(quantity) + 1);
+
+const mockDecrement = vi
+  .fn()
+  .mockImplementation((quantity) => parseInt(quantity) - 1);
+
+const mockUpdateQuantity = vi
+  .fn()
+  .mockImplementation((quantity, valueChanger) => {
+    valueChanger === "+"
+      ? (quantity = parseInt(quantity) + 1)
+      : (quantity = parseInt(quantity) - 1);
+  });
 
 describe("Shopping Cart Component", () => {
   it("Cart Component Renders", () => {
@@ -10,35 +26,76 @@ describe("Shopping Cart Component", () => {
     expect(render(screen.getByText(/checkout/i))).toBeInTheDocument();
   });
 
-  it("Total price component renders", () => {
-    expect(true).toBeFalsy(); //placeholder
-  });
-
   it("When Cart is not empty, selected item is rendered", () => {
-    expect(true).toBeFalsy(); //placeholder
+    render(<ShoppingCart items={notEmpty} />); //placeholder prop
+    expect(screen.getAllByTestId("items-wrapper")).toBeInTheDocument();
   });
 
   it("When Cart is empty shopping cart is blank", () => {
-    expect(true).toBeFalsy(); //placeholder
+    render(<ShoppingCart items={empty} />); //placeholder prop
+    expect(screen.getAllByTestId("items-wrapper")).not.toBeInTheDocument();
   });
 
-  it("Increasing item quantity increases total price", () => {
-    expect(true).toBeFalsy(); //placeholder
+  it("Increasing item quantity increases total price", async () => {
+    const user = userEvent.setup();
+    render(<ShoppingCart />);
+
+    await user.click("insert appropriate btn here").then(
+      () => expect(true).toBeFalsy() //placeholder
+    );
   });
 
-  it("Decreasing item quantity decreases total price", () => {
-    expect(true).toBeFalsy(); //placeholder
+  it("Decreasing item quantity decreases total price", async () => {
+    const user = userEvent.setup();
+    render(<ShoppingCart />);
+
+    await user.click("insert appropriate btn here").then(
+      () => expect(true).toBeFalsy() //placeholder
+    );
   });
 
-  it("Pressing '+' or '-' button changes quantity of item", () => {
-    expect(true).toBeFalsy(); //placeholder
+  it("Pressing '+' or '-' button changes quantity of item", async () => {
+    const user = userEvent.setup();
+    render(<ShoppingCart items={notEmpty} />); //placeholder prop
+
+    const itemsWrapper = screen.getByTestId("items-wrapper");
+
+    await user.click("insert appropriate btn here").then(() => {
+      let currentQuantity =
+        itemsWrapper.childNodes[0].querySelector(".quantity").textContent;
+      const plusOne = mockIncrement(currentQuantity);
+
+      expect(
+        itemsWrapper.childNodes[0].querySelector(".quantity").textContent
+      ).toEqual(plusOne);
+    });
+
+    await user.click("insert appropriate btn here").then(() => {
+      let currentQuantity =
+        itemsWrapper.childNodes[0].querySelector(".quantity").textContent;
+
+      const minusOne = mockDecrement(currentQuantity);
+      expect(
+        itemsWrapper.childNodes[0].querySelector(".quantity").textContent
+      ).toEqual(minusOne);
+    });
   });
 
-  it("Pressing '-' button to reach quantity 0 remmoves item from cart", () => {
-    expect(true).toBeFalsy(); //placeholder
+  it("Pressing '-' button to reach quantity 0 remmoves item from cart", async () => {
+    const user = userEvent.setup();
+    render(<ShoppingCart />);
+
+    await user.click("insert appropriate btn here").then(
+      () => expect(true).toBeFalsy() //placeholder
+    );
   });
 
-  it("Trash can button removes item from cart", () => {
-    expect(true).toBeFalsy(); //placeholder
+  it("Trash can button removes item from cart", async () => {
+    const user = userEvent.setup();
+    render(<ShoppingCart />);
+
+    await user.click("insert appropriate btn here").then(
+      () => expect(true).toBeFalsy() //placeholder
+    );
   });
 });

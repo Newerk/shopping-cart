92f837dab9b9f015cd010ec3301b3d0285402f79
newerk <juniusowens@yahoo.com>
2023-12-06 11:31:25 -0500

    Fix Tests to See of Item Cards Have all Required Components

--- src/components/item-card/item-card.test.jsx
+++ src/components/item-card/item-card.test.jsx
@@ -1,15 +1,23 @@
 import { render } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
+import { ShopPage } from "../shop-page/shop-page";
+import { mockDataBase } from "../../mock-database";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Card Component", () => {
   it("render card", () => {
-    const component = render(<Card />);
-    expect(component).toBeInTheDocument();
-    expect(component.getByAltText("item_thumbnail")).toBeInTheDocument();
-    expect(component.getByTestId("item_name")).toBeInTheDocument();
-    expect(component.getByTestId("item_price")).toBeInTheDocument();
+    const component = render(
+      <ShopPage database={mockDataBase} setCartSize={vi.fn()} />,
+      { wrapper: BrowserRouter }
+    );
+
+    expect(component.getAllByAltText("item_thumbnail").length).toBeGreaterThan(
+      0
+    );
+    expect(component.getAllByTestId("item_name").length).toBeGreaterThan(0);
+    expect(component.getAllByTestId("item_price").length).toBeGreaterThan(0);
     expect(
-      component.getByRole("button", { name: /add to cart/i })
-    ).toBeInTheDocument();
+      component.getAllByRole("button", { name: /add to cart/i }).length
+    ).toBeGreaterThan(0);
   });
 });
